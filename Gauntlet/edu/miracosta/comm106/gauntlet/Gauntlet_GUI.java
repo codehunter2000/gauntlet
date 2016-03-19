861fe2bdb97efa2c25931face1ccea37c1fc9c8c
codehunter2000 <gabrielrhunt@gmail.com>
2016-03-19 15:54:20 -0700

    Finally added image to main panel.

--- Gauntlet/edu/miracosta/comm106/gauntlet/Gauntlet_GUI.java
+++ Gauntlet/edu/miracosta/comm106/gauntlet/Gauntlet_GUI.java
@@ -1,12 +1,14 @@
 package edu.miracosta.comm106.gauntlet;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.event.*;
-import java.io.Serializable;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class Gauntlet_GUI
 {
-	
 	private static final int HEIGHT = 300;
 	private static final int WIDTH = 400;
 	private JMenuBar menuBar;
@@ -15,32 +17,40 @@ public class Gauntlet_GUI
 	private JFrame mainFrame;
 	private JPanel mainPanel;
 	private JLabel imageLabel;
-	private ImageIcon eye = new ImageIcon("images/The-Illuminati-Eye.png");
+	private File illuminati;
+
 	
 	public Gauntlet_GUI()
 	{
-		mainFrame = new JFrame("GAUNTLET");
-		mainFrame.setSize(WIDTH, HEIGHT);
-		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		menuBar = new JMenuBar();
-		close = new JMenu("Exit");
-		exit = new JMenuItem("Exit");
-		exit.addActionListener(new exitButtonListener());
-		buildMainPanel();
-		close.add(exit);
-		menuBar.add(close);
-		mainFrame.setJMenuBar(menuBar);
-		mainFrame.add(mainPanel);
-		mainFrame.pack();
-		mainFrame.setVisible(true);
+		
+		BufferedImage eye;
+			// Will need to be changed for windows filepath
+		illuminati = new File("/home/gabriel/git/gauntlet/Gauntlet/edu/miracosta/comm106/gauntlet/images/The-Illuminati-Eye.png");
+		
+		if (illuminati.exists())
+			System.out.println("File exists");
+		if (!illuminati.exists())
+			System.out.println("DNE");
+		
+		try 
+		{
+			eye = ImageIO.read(illuminati);
+			mainFrame = new JFrame("GAUNTLET");
+			mainFrame.setSize(WIDTH, HEIGHT);
+			mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			imageLabel = new JLabel(new ImageIcon(eye));
+			mainFrame.add(imageLabel);
+			mainFrame.setVisible(true);
+		} 
+		
+		catch (IOException e) 
+		{
+			System.out.println("Image Not Found.");
+		}
+		
+		
 	}
-	
-	public void buildMainPanel()
-	{
-		imageLabel = new JLabel(eye);
-		mainPanel = new JPanel();
-		mainPanel.add(imageLabel);
-	}
+
 	
 	private class exitButtonListener implements ActionListener
 	{
